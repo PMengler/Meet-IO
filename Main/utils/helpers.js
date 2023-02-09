eeb8a5081e050ba795897efdb3eff50f08ba5dfd
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-02-08 21:40:23 -0800

    adding emojis array

--- Main/utils/helpers.js
+++ Main/utils/helpers.js
@@ -2,4 +2,10 @@ module.exports = {
   format_date: (date) => {
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   },
+  get_emoji: (index) => {
+    const emojis = ['🐶', '🐱', '🐭', '🐹', '🐰', '🦊', '🐻'];
+    const emoji = emojis[index];
+
+    return `<span for="img" aria-label="emoji">${emoji}</span>`;
+  },
 };
