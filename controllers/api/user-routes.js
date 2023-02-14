ad777e73c719a501e1d31e2f8b0b77f8cb42f381
PMengler <114950259+PMengler@users.noreply.github.com>
2023-02-14 13:23:30 -0800

    working on login, logout, and signup functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,70 +1,47 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    const userData = await User.findAll();
-
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-// router.post('/', async (req, res) => {
-//   try {
-//     const userData = await User.create(req.body);
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that username!' });
+        return;
+      }
+      const validPassword = dbUserData.checkPassword(req.body.password);
 
-//     req.session.save(() => {
-//       req.session.user_id = userData.id;
-//       req.session.logged_in = true;
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
 
-//       res.status(200).json(userData);
-//     });
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
-// router.post('/login', async (req, res) => {
-//   try {
-//     const userData = await User.findOne({ where: { email: req.body.email } });
-
-//     if (!userData) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password, please try again' });
-//       return;
-//     }
-
-//     const validPassword = await userData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect email or password, please try again' });
-//       return;
-//     }
-
-//     req.session.save(() => {
-//       req.session.user_id = userData.id;
-//       req.session.logged_in = true;
-
-//       res.json({ user: userData, message: 'You are now logged in!' });
-//     });
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
-// router.post('/logout', (req, res) => {
-//   if (req.session.logged_in) {
-//     req.session.destroy(() => {
-//       res.status(204).end();
-//     });
-//   } else {
-//     res.status(404).end();
-//   }
-// });
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- controllers/home-route.js
+++ controllers/home-route.js
@@ -3,10 +3,10 @@ const { User, Event } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    // req.session.loggedIn = true;
     if (!req.session.loggedIn) {
       res.redirect('/login');
-    } else{
+      return;
+    } else {
       res.render('homepage');
     }
   } catch (err) {
@@ -14,29 +14,35 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      include: {
-        model: Event,
-      },
-    });
-    const users = userData.map((user) => user.get({plain: true}));
-    res.render('all', {users});
-    // res.status(200).json(userData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
+// router.get('/users', async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       include: {
+//         model: Event,
+//       },
+//     });
+//     const users = userData.map((user) => user.get({ plain: true }));
+//     res.render('all', { users });
+//     // res.status(200).json(userData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 router.get('/login', (req, res) => {
-  res.render('login');
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login', { loggedIn: req.session.loggedIn });
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
 router.get('/calendar', (req, res) => {
-
-  res.render('calendar'); 
+  res.render('calendar');
 });
 
 module.exports = router;
