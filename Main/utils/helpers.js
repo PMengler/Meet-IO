44979682cf8dcb9154d574c0236fa53212e551c0
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-14 10:54:58 -0800

    Script file now linked to the calender handlebar.

--- Main/utils/helpers.js
+++ Main/utils/helpers.js
@@ -1,4 +1,6 @@
 module.exports = {
+
+  // Date related methods
   format_date: (date) => {
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   },
@@ -8,11 +10,5 @@ module.exports = {
     const emoji = emojis[index];
 
     return `<span for="img" aria-label="emoji">${emoji}</span>`;
-},
-  next: () => { 
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    return showCalendar(currentMonth, currentYear);
-},
-
+  },
 };
