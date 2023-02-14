3d41b1a20eb0fcfc8175e9eacd51c550cc816b8a
Nick Morris <nicholasmorris2599@gmail.com>
2023-02-14 15:59:27 -0800

    Added event listener to the send button for the chat. This is currently logging to the console but will impliment this into the chat field.

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,6 +1,17 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
 // Socket picks up on the backend server via 'message' tag and displays the message from our backend server
 socket.on('message', (msg) => {
+    console.log(msg);
+});
+
+// Message submit
+chatForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const msg = event.target.elements.msg.value;
+
     console.log(msg);
 });
