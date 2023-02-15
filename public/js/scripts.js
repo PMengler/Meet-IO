d8f9d4ef964465347caccc089d205ca551a7dbad
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-02-14 20:06:46 -0800

    added events details, added navbar, added navbar style

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -81,3 +81,11 @@ function showCalendar(month, year) {
     }
 
 }
+
+    //mobile menu
+    const burgerIcon = document.querySelector('#burger');
+    const navbarMenu = document.querySelector('#nav-links');
+
+    burgerIcon.addEventListener('click', () => {
+        navbarMenu.classList.toggle('is-active');
+    });
