8f2b885fcbd55ca7d76e0940b978c34321b78bc7
PMengler <114950259+PMengler@users.noreply.github.com>
2023-02-14 16:41:34 -0800

    created dashboard routes, handlebars and am showing calendar

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-route');
+const dashboardRoutes = require('./dashboard-route');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
