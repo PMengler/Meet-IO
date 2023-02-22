f71d32a06b107841fc8e1bb31cce002a6c59464e
A P <ashleighpaschal@gmail.com>
2023-02-21 17:07:45 -0800

    modified:   controllers/google-api/index.js

--- controllers/google-api/index.js
+++ controllers/google-api/index.js
@@ -8,7 +8,7 @@ dotenv.config()
 const app = express();
 const apiRoutes = require('./google-api')
 const {google} = require('googleapis');
-//const calendar = require(
+
 
 const PORT = process.env.PORT || 3001;
 // Invoke 
@@ -64,30 +64,30 @@ module.exports = router;
 
 
 
-  
+//} 
     // Create <form> element to submit parameters to OAuth 2.0 endpoint.
-    var form = document.createElement('form');
-    form.setAttribute('method', 'GET'); // Send as a GET request.
-    form.setAttribute('action', oauth2Endpoint);
+   // var form = document.createElement('form');
+   // form.setAttribute('method', 'GET'); // Send as a GET request.
+   // form.setAttribute('action', oauth2Endpoint);
   
     // Parameters to pass to OAuth 2.0 endpoint.
-    var params = {'client_id': 'YOUR_CLIENT_ID',
-                  'redirect_uri': 'YOUR_REDIRECT_URI',
-                  'response_type': 'token',
-                  'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
-                  'include_granted_scopes': 'true',
-                  'state': 'pass-through value'};
+    //var params = {'client_id': 'YOUR_CLIENT_ID',
+   //               'redirect_uri': 'YOUR_REDIRECT_URI',
+   //               'response_type': 'token',
+   //               'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+   //               'include_granted_scopes': 'true',
+   //               'state': 'pass-through value'};
   
     // Add form parameters as hidden input values.
-    for (var p in params) {
-      var input = document.createElement('input');
-      input.setAttribute('type', 'hidden');
-      input.setAttribute('name', p);
-      input.setAttribute('value', params[p]);
-      form.appendChild(input);
-    }
+   // for (var p in params) {
+   //   var input = document.createElement('input');
+   //   input.setAttribute('type', 'hidden');
+   //   input.setAttribute('name', p);
+   //   input.setAttribute('value', params[p]);
+   //   form.appendChild(input);
+   // }
   
     // Add form to page and submit it to open the OAuth 2.0 endpoint.
-    document.body.appendChild(form);
-    form.submit();
-  }
+   // document.body.appendChild(form);
+   // form.submit();
+//}
